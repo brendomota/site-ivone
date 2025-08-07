1e8df3f6206eb66665413f7f2a2e8fa3b2a5a05d
brendomota <brendo.saescola@gmail.com>
2025-08-07 10:56:20 -0300

    update(App.tsx): atualizar App.tsx para que ao atualizar a página, o usuário volte ao topo dela.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css'
 import Home from './pages/Home'
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [])
+
   return (
     <>
       <Home />
